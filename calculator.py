41743a057ab45cf66cc811a9be8e7fbbf1375d3e
lokeshwari dulla <dudalalokeshwari345@gmail.com>
2026-06-15 06:38:57 +0530

    completed calculator project

--- calculator.py
+++ calculator.py
@@ -1,3 +1,10 @@
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
 print("Sum =", a + b)
+print("Difference =", a - b)
+print("Product =", a * b)
+if b != 0:
+    print("Quotient =", a / b)
+else:
+    print("Cannot divide by zero")
+        
